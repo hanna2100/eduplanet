24df4c268f113cf7ccf578ea27f781153d7cf064
haneeea <haneeea@gmail.com>
2020-02-17 09:51:45 +0900

    header fix 2

--- academy_header.php
+++ academy_header.php
@@ -7,7 +7,7 @@
     <title>에듀플래닛</title>
 
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/eduplanet/academy_header_soyoung.css">
+    <link rel="stylesheet" href="/eduplanet/academy_header.css">
 
 </head>
 
--- academy_header_include.php
+++ academy_header_include.php
@@ -7,7 +7,7 @@
     <title>에듀플래닛</title>
 
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/eduplanet/academy_header_soyoung_new.css">
+    <link rel="stylesheet" href="/eduplanet/academy_header_include.css">
 
 </head>
 
