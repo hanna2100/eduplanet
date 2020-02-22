d16b9b9d9e9bc75dcccd5d4ee9df453ae1af8c8a
haneeea <haneeea@gmail.com>
2020-02-23 08:10:41 +0900

    header edit

--- academy/header/academy_header.php
+++ academy/header/academy_header.php
@@ -7,7 +7,7 @@
     <title>에듀플래닛</title>
 
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/eduplanet/academy_header.css">
+    <link rel="stylesheet" href="/eduplanet/academy/header/academy_header.css">
 
 </head>
 
