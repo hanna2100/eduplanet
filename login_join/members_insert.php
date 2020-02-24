0735a8520bd2112547dc19a3f41b8a58bd36b910
eunhye <gracepark427@gmail.com>
2020-02-24 19:31:32 +0900

    file upload 추가

--- login_join/members_insert.php
+++ login_join/members_insert.php
@@ -8,6 +8,7 @@
   $pw = test_input($_POST['inputPw1']);
   $email = test_input($_POST['inputEmail']);
 
+
   if($mode == "gm"){
     $tel = test_input($_POST['inputTel']);
     $age = test_input($_POST['inputAge']);
@@ -19,15 +20,43 @@
     $no = "gm_no";
 
   }else if($mode == "am"){
+    // 2. 파일
+    $upload_dir = '../data/bsnsLic/';
+    $upfile_name = $_FILES["inputLicense"]["name"]; // 사용자가 실제 올린 파일 이름
+    $upfile_tmp_name = $_FILES["inputLicense"]["tmp_name"];  // 서버가 임의로 준 임시 파일 이름
+    $upfile_type = $_FILES["inputLicense"]["type"];
+    $upfile_size = $_FILES["inputLicense"]["size"];
+    $upfile_error = $_FILES["inputLicense"]["error"];
+
+    if($upfile_name && !$upfile_error){
+      $file = explode(".", $upfile_name); // 사용자가 올린 파일을 이름과 확장자로 나눈다
+      $file_name = $file[0];
+      $file_ext = $file[1];
+      $new_file_name = date("y_m_d_h_i_s");
+      $new_file_name = $new_file_name."_".$file_name;  // 현재 시간과 파일 이름을 붙여서 새로운 임시 이름을 만든다
+      $copied_file_name = $new_file_name.".".$file_ext;  // 새로 만든 임시 이름에 확장자를 붙인다
+      $uploaded_file = $upload_dir.$copied_file_name;  // 파일을 저장할 경로를 앞에 붙인다
+
+      if($upfile_size > 1000000){
+        alert_back('업로드 파일 크기가 지정된 용량(1MB)을 초과합니다!<br>파일 크기를 체크해주세요!');
+      }
+      if(!move_uploaded_file($upfile_tmp_name, $uploaded_file)){
+        alert_back('파일을 지정한 디렉토리에 복사하는데 실패했습니다.');
+      }
+    }else{
+          echo ("<script>alert('파일없음');</script>");
+      $real_file_name = "";
+      $real_file_type = "";
+      $copied_file_name = "";
+    }
+
     $acdName = test_input($_POST['inputAcdName']);
     $rprsn = test_input($_POST['inputRprsn']);
-    $license = test_input($_POST['inputLicense']);
 
     $sql_insert = "insert into a_members values
-            (null, 0, '$id', '$pw', '$email', '$acdName', '$rprsn', 'file_copy.jpg', 'N', '0000-00-00', '$today');";
+            (null, 0, '$id', '$pw', '$email', '$acdName', '$rprsn', '$copied_file_name', 'N', '0000-00-00', '$today');";
     $sql_select = "select * from a_members where id='$id'";
     $no = "am_no";
-
   }
 
   mysqli_query($conn, $sql_insert);
@@ -40,10 +69,10 @@
   echo "
         <script>
             alert('에듀플래닛에 오신 것을 환영합니다!');
-
+            location.href = '/eduplanet/index.php';
         </script>
       ";
 
-  // location.href = '/eduplanet/index.php';
+
 
  ?>
