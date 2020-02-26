2ccaa75e460643ea284c83084cb11135aced9c6c
eunhye <gracepark427@gmail.com>
2020-02-26 10:42:37 +0900

    section 하단 이용안내 css 수정

--- login_join/login_form.php
+++ login_join/login_form.php
@@ -7,6 +7,8 @@
     <link rel="shortcut icon" href="/eduplanet/img/favicon.png">
     <script src="http://code.jquery.com/jquery-1.12.4.min.js" charset="utf-8"></script>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&amp;display=swap" rel="stylesheet">
+    <!-- 카카오 로그인 -->
+    <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
     <link rel="stylesheet" href="./login.css">
   </head>
 
@@ -26,6 +28,14 @@
          <h2> 일반회원 로 그 인</h2>
           <div id="form_wrapper">
            <form id="form_login" name="login_form" action="<?=$action?>" method="post">
+
+             <!-- <div id="kakao_btn_changed"> -->
+               <!-- <a id="kakao-login-btn"></a> -->
+               <!-- <a href="http://developers.kakao.com/logout"></a> -->
+              <!-- <a id="custom-login-btn" href="javascript:loginWithKakao()">
+              <img src="../img/Kakao_login_btn.png" width="300"/></a> -->
+             <!-- </div> -->
+
              <div class="formBox">
                <label for="inputId">아이디&nbsp;&nbsp;&nbsp;</label>
                <input type="text" class="formInput" id="inputId" name="inputId" required>
@@ -73,6 +83,73 @@
        $(".join").html("<a href='./join_form.php?mode=am'>학원회원 가입</a>");
        $(".login").prev().html("일반 회원이시라면");
        $(".login").html("<a href='./login_form.php?mode=gm'>일반회원 로그인</a>");
+
+       // 카카오 script key 입력
+  Kakao.init('03be234663870819293c389394e57405');
+  Kakao.Auth.createLoginButton({
+    container: '#kakao-login-btn',
+    success: function(authObj) {
+      Kakao.API.request({
+        url : '/v2/user/me',
+        success: function(res){
+          alert(JSON.stringify(res)); //<---- kakao.api.request 에서 불러온 결과값 json형태로 출력
+          alert(JSON.stringify(authObj)); //<----Kakao.Auth.createLoginButton에서 불러온 결과값 json형태로 출력
+          // console.log(res.id);//<---- 콘솔 로그에 id 정보 출력(id는 res안에 있기 때문에  res.id 로 불러온다)
+          console.log(res.kakao_account.email);//<---- 콘솔 로그에 email 정보 출력 (어딨는지 알겠죠?)
+          console.log(res.kakao_account.birthday);//<---- 콘솔 로그에 email 정보 출력 (어딨는지 알겠죠?)
+          console.log(res.properties.nickname);//<---- 콘솔 로그에 닉네임 출력(properties에 있는 nickname 접근
+      // res.properties.nickname으로도 접근 가능 )
+          // console.log(authObj.access_token);//<---- 콘솔 로그에 토큰값 출력
+          // console.log(res.properties['birthday']);
+          var id = res.id;
+          var name = res.properties.nickname;
+          console.log(id);
+        }
+      })
+    },
+    fail: function(err) {
+       alert(JSON.stringify(err));
+    }
+  });
+
+ // 로그인 처리
+// function loginWithKakao(){
+//   console.log("eee");
+//     Kakao.Auth.cleanup();
+//     Kakao.Auth.login({
+//         persistAccessToken: true,
+//         persistRefreshToken: true,
+//         success: function(authObj) {
+//           Kakao.API.request({
+//             url : '/v2/user/me',
+//             success: function(res){
+//               alert(JSON.stringify(res)); //<---- kakao.api.request 에서 불러온 결과값 json형태로 출력
+//               alert(JSON.stringify(authObj)); //<----Kakao.Auth.createLoginButton에서 불러온 결과값 json형태로 출력
+//               // console.log(res.id);//<---- 콘솔 로그에 id 정보 출력(id는 res안에 있기 때문에  res.id 로 불러온다)
+//               console.log(res.kakao_account.email);//<---- 콘솔 로그에 email 정보 출력 (어딨는지 알겠죠?)
+//               console.log(res.kakao_account.birthday);//<---- 콘솔 로그에 email 정보 출력 (어딨는지 알겠죠?)
+//               console.log(res.properties.nickname);//<---- 콘솔 로그에 닉네임 출력(properties에 있는 nickname 접근
+//           // res.properties.nickname으로도 접근 가능 )
+//               // console.log(authObj.access_token);//<---- 콘솔 로그에 토큰값 출력
+//               // console.log(res.properties['birthday']);
+//               var id = res.id;
+//               var name = res.properties.nickname;
+//               console.log(id);
+//               // setCookie("kakao_login","done",1); // 쿠키생성 (로그인)
+//               //alert(cookiedata);
+//               // createLogoutKakao();
+//               window.location.href="../index.php";
+//             }
+//           })
+//         },
+//             fail: function(err) {
+//               alert(JSON.stringify(err));
+//         }
+//     });
+// }
+
+
+
      }
    </script>
 
