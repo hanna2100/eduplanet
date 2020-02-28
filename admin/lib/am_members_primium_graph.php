bf6c6ab66d9d96b3acee3451f71f5b18df529895
hanna2100 <hanna0497@naver.com>
2020-02-28 11:51:55 +0900

    관리자페이지 세션추가
    관리자페이지 세션추가

--- admin/lib/am_members_primium_graph.php
+++ admin/lib/am_members_primium_graph.php
@@ -1,15 +1,15 @@
 <?php
-// session_start();
-// $is_admin = isset($_SESSION["admin"])? $_SESSION["admin"]: 0 ;
-// if ( $is_admin != 1 ){
-//     echo("
-//         <script>
-//         alert('관리자 전용 페이지 입니다.');
-//         history.go(-1)
-//         </script>
-//     ");
-//     exit;
-// }
+    include $_SERVER['DOCUMENT_ROOT'] . "/eduplanet/lib/session_start.php";
+
+    if ($admin== "" ){
+    echo("
+        <script>
+        alert('관리자 전용 페이지 입니다.');
+        history.go(-1)
+        </script>
+    ");
+    exit;
+    }
 
 $y   = $_POST['y'];
 $m   = $_POST['m'];
