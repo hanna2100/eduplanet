d2b4bb0b99baee5725212ed6f3fea36dbe65c89b
haneeea <haneeea@gmail.com>
2020-02-28 17:22:27 +0900

    slider fix

--- index/index_content.php
+++ index/index_content.php
@@ -29,58 +29,57 @@
                     <p>에듀플래닛을 가입하고 나의 성공시대 시작됐다 야옹야옹</p>
                 </div>
             </div>
-        </div>
-        <!-- 슬라이더 end -->
 
-        <form name="academy_search_form" action="#" method="POST">
+            <form name="academy_search_form" action="#" method="POST">
 
-            <div class="index_main_wrap">
-                <div class="index_main_title">
-                    <div class="index_main_search">
+                <div class="index_main_wrap">
+                    <div class="index_main_title">
+                        <div class="index_main_search">
 
-                        <select name="select_district" id="select_district">
-                            <option selected value="">시/군 선택</option>
-                            <option value="가평군">가평군</option>
-                            <option value="고양시">고양시</option>
-                            <option value="과천시">과천시</option>
-                            <option value="광명시">광명시</option>
-                            <option value="광주시">광주시</option>
-                            <option value="구리시">구리시</option>
-                            <option value="군포시">군포시</option>
-                            <option value="김포시">김포시</option>
-                            <option value="남양주시">남양주시</option>
-                            <option value="동두천시">동두천시</option>
-                            <option value="부천시">부천시</option>
-                            <option value="성남시">성남시</option>
-                            <option value="수원시">수원시</option>
-                            <option value="시흥시">시흥시</option>
-                            <option value="안산시">안산시</option>
-                            <option value="안성시">안성시</option>
-                            <option value="안양시">안양시</option>
-                            <option value="양주시">양주시</option>
-                            <option value="양평군">양평군</option>
-                            <option value="여주시">여주시</option>
-                            <option value="연천군">연천군</option>
-                            <option value="오산시">오산시</option>
-                            <option value="용인시">용인시</option>
-                            <option value="의왕시">의왕시</option>
-                            <option value="의정부시">의정부시</option>
-                            <option value="이천시">이천시</option>
-                            <option value="파주시">파주시</option>
-                            <option value="평택시">평택시</option>
-                            <option value="포천시">포천시</option>
-                            <option value="하남시">하남시</option>
-                            <option value="화성시">화성시</option>
-                        </select>
+                            <select name="select_district" id="select_district">
+                                <option selected value="">시/군 선택</option>
+                                <option value="가평군">가평군</option>
+                                <option value="고양시">고양시</option>
+                                <option value="과천시">과천시</option>
+                                <option value="광명시">광명시</option>
+                                <option value="광주시">광주시</option>
+                                <option value="구리시">구리시</option>
+                                <option value="군포시">군포시</option>
+                                <option value="김포시">김포시</option>
+                                <option value="남양주시">남양주시</option>
+                                <option value="동두천시">동두천시</option>
+                                <option value="부천시">부천시</option>
+                                <option value="성남시">성남시</option>
+                                <option value="수원시">수원시</option>
+                                <option value="시흥시">시흥시</option>
+                                <option value="안산시">안산시</option>
+                                <option value="안성시">안성시</option>
+                                <option value="안양시">안양시</option>
+                                <option value="양주시">양주시</option>
+                                <option value="양평군">양평군</option>
+                                <option value="여주시">여주시</option>
+                                <option value="연천군">연천군</option>
+                                <option value="오산시">오산시</option>
+                                <option value="용인시">용인시</option>
+                                <option value="의왕시">의왕시</option>
+                                <option value="의정부시">의정부시</option>
+                                <option value="이천시">이천시</option>
+                                <option value="파주시">파주시</option>
+                                <option value="평택시">평택시</option>
+                                <option value="포천시">포천시</option>
+                                <option value="하남시">하남시</option>
+                                <option value="화성시">화성시</option>
+                            </select>
 
-                        <form name="search_academy_form" action="#" method="post">
-                            <input placeholder="학원 이름으로 검색" type="text" name="acd_name" id="acd_name_index">
-                            <button id="button_main_search" type="button">검색</button>
-                        </form>
+                            <form name="search_academy_form" action="#" method="post">
+                                <input placeholder="학원 이름으로 검색" type="text" name="acd_name" id="acd_name_index">
+                                <button id="button_main_search" type="button">검색</button>
+                            </form>
 
+                        </div>
                     </div>
                 </div>
-            </div>
+        </div>
     </div>
 
     <div class="index_content_wrap">
