5296663da5afcda31f289d0264fada67e2dad2a7
haneeea <haneeea@gmail.com>
2020-03-02 14:43:45 +0900

    withdrawal DB insert

--- mypage/member_delete.php
+++ mypage/member_delete.php
@@ -4,19 +4,24 @@
 
     include_once $_SERVER["DOCUMENT_ROOT"]."/eduplanet/lib/db_connector.php";
 
+    // 아이디 가져오기
     if (isset($_POST["inputId"])) {
         $inputId = $_POST["inputId"];
     }
 
+    // 비밀번호 가져오기
     if (isset($_POST["inputPw1"])) {
         $inputPw = $_POST["inputPw1"];
     }
 
+    // 테이블 설정하기
     if ($gm_no) {
         $table = "g_members";
+        $type = "G";
 
     } else if ($am_no) {
         $table = "a_members";
+        $type = "A";
 
     } else {
         echo "
@@ -27,11 +32,16 @@
             ";
     }
 
-    // 비밀번호 가져오기
+    // DB에서 회원정보 가져오기
     $sql = "SELECT * FROM $table WHERE id='$inputId'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
+
+    $no = $row["no"];
     $password = $row["pw"];
+    $regist_day = $row["regist_day"];
+
+    $today = date("Y-m-d");
 
     // 비밀번호가 일치하지 않을 때
     if ($inputPw != $password) {
@@ -48,11 +58,16 @@
     // 비밀번호가 일치할 때
     } else if ($inputPw == $password) {
 
-        // 삭제 쿼리문 실행
+        // 회원 테이블에서 DELETE
         $sql = "DELETE FROM $table WHERE id='$inputId'";
         mysqli_query($conn, $sql);
 
-        // session_start();
+        // 탈퇴회원 테이블에 INSERT
+        $sql_drawal = "INSERT INTO withdrawal(type, mmbr_no, join_date, wthd_date)";
+        $sql_drawal .= "VALUES('$type', '$no', '$regist_day', '$today')";
+        mysqli_query($conn, $sql_drawal);
+
+        // 세션 초기화 (자동 로그아웃)
         unset($_SESSION["am_no"]);
         unset($_SESSION["gm_no"]);
         unset($_SESSION["pam_no"]);
