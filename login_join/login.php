1606c377093601f8fe4662806b704c88ad61b299
eunhye <gracepark427@gmail.com>
2020-03-03 16:33:34 +0900

    사업자 회원 승인여부 검사하여 로그인 가/부 결정

--- login_join/login.php
+++ login_join/login.php
@@ -14,7 +14,7 @@
 
   $mode = $_GET['mode'];
 
-  
+
 
   // 관리자 아이디, 비밀번호 입력 시 mode 설정
   if ($input_id == ADMIN_ID && $input_pw == ADMIN_PW) {
@@ -54,25 +54,40 @@
   $pw = $row["pw"];
   $expiry_day = $row["expiry_day"];
 
+
+  if($mode == "am"){
+    $acd_no = $row["acd_no"];
+
+    $sql_approval = "SELECT approval FROM a_members WHERE acd_no=$acd_no; ";
+    $result_approval = mysqli_query($conn, $sql_approval);
+    $row_approval = mysqli_fetch_array($result_approval);
+
+    $approval = $row_approval['approval'];
+  }
+
   $sql_order = "SELECT EXISTS (SELECT date FROM $table_order WHERE $mode_no='$no' order by date desc) as success;";
 
 
   // DB 에서 가져온 아이디가 없을 때
   if (!$id_exist) {
 
-    alert_back('아이디가 존재하지 않습니다.');
-    $input_id = "";
+      alert_back('아이디가 존재하지 않습니다.');
+      $input_id = "";
 
   // DB 에서 가져온 아이디가 있을 때, 비밀번호 검사 후 세션값 주기
-  } else {
+  } else if ($id_exist && $input_pw != $pw) {
 
-    if ($input_pw != $pw) {
       alert_back('비밀번호가 일치하지 않습니다.');
 
-    } else {
+  } else if ($mode == "am" && $approval == "N"){
+
+      alert_move('현재 승인절차가 진행중입니다. 승인 후 로그인 가능합니다.', '/eduplanet/index.php');
+
+  } else {
 
       // 만료날짜가 없을 때 (유료회원이 아닐 때)
       if ($expiry_day == "0000-00-00") {
+
 
         // 기존에 멤버십 구입을 한번도 하지 않은 경우
         $result_order = mysqli_query($conn, $sql_order);
@@ -95,14 +110,6 @@
         }
         // 기존에 멤버십 구입을 했으나 만료된 경우
         else{
-
-          // 테이블 만료날짜를 0000-00-00 으로 업데이트
-          // $sql_exp = "UPDATE $table_members SET expiry_day='0000-00-00' WHERE no=$no";
-          // mysqli_query($conn, $sql_exp);
-          //
-          // if (!mysqli_query($conn, $sql_exp)) {
-          //   echo "오류ㅠ.ㅠ : ".mysqli_error($conn);
-          // }
 
           // gm_no, am_no 세션 값 주기
           $_SESSION[$mode_no] = $no;
@@ -172,7 +179,7 @@
       mysqli_close($conn);
 
     }
-  }
+  
 
   function alert_move($msg, $url){
     echo "
