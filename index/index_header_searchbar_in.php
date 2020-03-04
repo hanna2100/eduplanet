4f17c6ba1ea10fc81dfa11ae136c1905994c2698
haneeea <haneeea@gmail.com>
2020-03-04 18:58:37 +0900

    searchbar fix

--- index/index_header_searchbar_in.php
+++ index/index_header_searchbar_in.php
@@ -60,7 +60,7 @@
                     function searchAcademy() {
 
                         // 검색어를 입력하지 않았을 때
-                        if (document.getElementById("acd_name_index").value == "") {
+                        if (document.getElementById("acd_name_in").value == "") {
 
                             // 지역을 선택하지 않았을 때
                             if (document.getElementById("select_district").value == "") {
@@ -72,15 +72,15 @@
                             }
 
                             // 검색어를 입력했을 때
-                        } else if (document.getElementById("acd_name_index").value != "") {
+                        } else if (document.getElementById("acd_name_in").value != "") {
 
                             // 지역을 선택하지 않았을 때
                             if (document.getElementById("select_district").value == "") {
-                                location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_index").value;
+                                location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_in").value;
 
                                 // 지역을 선택했을 때
                             } else if (document.getElementById("select_district").value != "") {
-                                location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_index").value + '&district=' + document.getElementById("select_district").value;
+                                location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_in").value + '&district=' + document.getElementById("select_district").value;
                             }
 
                         }
--- index/index_header_searchbar_out.php
+++ index/index_header_searchbar_out.php
@@ -249,7 +249,7 @@
                         function searchAcademy() {
 
                             // 검색어를 입력하지 않았을 때
-                            if (document.getElementById("acd_name_index").value == "") {
+                            if (document.getElementById("acd_name_out").value == "") {
 
                                 // 지역을 선택하지 않았을 때
                                 if (document.getElementById("select_district").value == "") {
@@ -261,15 +261,15 @@
                                 }
 
                                 // 검색어를 입력했을 때
-                            } else if (document.getElementById("acd_name_index").value != "") {
+                            } else if (document.getElementById("acd_name_out").value != "") {
 
                                 // 지역을 선택하지 않았을 때
                                 if (document.getElementById("select_district").value == "") {
-                                    location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_index").value;
+                                    location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_out").value;
 
                                     // 지역을 선택했을 때
                                 } else if (document.getElementById("select_district").value != "") {
-                                    location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_index").value + '&district=' + document.getElementById("select_district").value;
+                                    location.href = '/eduplanet/acd_list/view_all.php?search=' + document.getElementById("acd_name_out").value + '&district=' + document.getElementById("select_district").value;
                                 }
 
                             }
